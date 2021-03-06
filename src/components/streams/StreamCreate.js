00dd6eca206d4e90836f96a6393abd48e51f2eb5
rohan-04 <mathurrohan04@gmail.com>
2021-03-07 02:29:36 +0530

    form display error done

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,15 +2,27 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-	renderInput({ input, label, meta }) {
+	renderError({ error, touched }) {
+		if (touched && error) {
+			return (
+				<div className="ui error message">
+					<div className="header ">{error}</div>
+				</div>
+			);
+		}
+	}
+
+	renderInput = ({ input, label, meta }) => {
+		const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+
 		return (
-			<div className="field">
+			<div className={className}>
 				<label>{label}</label>
-				<input {...input} />
-				<div>{meta.error}</div>
+				<input {...input} autoComplete="off" />
+				{this.renderError(meta)}
 			</div>
 		);
-	}
+	};
 
 	onSubmit(formValues) {
 		console.log(formValues);
