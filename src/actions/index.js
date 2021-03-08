150b4db7d5ac93e53838882a9686eb8fca3c75bf
rohan-04 <mathurrohan04@gmail.com>
2021-03-09 00:47:11 +0530

    fetching all the streams done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,15 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT } from './types';
+import {
+	SIGN_IN,
+	SIGN_OUT,
+	CREATE_STREAM,
+	FETCH_STREAMS,
+	FETCH_STREAM,
+	DELETE_STREAM,
+	EDIT_STREAM,
+} from './types';
 
+// Sign In and out
 export const signIn = (userId) => {
 	return {
 		type: SIGN_IN,
@@ -14,6 +23,38 @@ export const signOut = () => {
 	};
 };
 
-export const createStream = (formValues) => async (dispatch) => {
-	streams.post('/streams', formValues);
+// To create stream
+export const createStream = (formValues) => async (dispatch, getState) => {
+	const { userId } = getState().auth;
+	const response = await streams.post('/streams', { ...formValues, userId });
+
+	dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+// To fetch all streams
+export const fetchStreams = () => async (dispatch) => {
+	const response = await streams.get('/streams');
+
+	dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+// To fetch one stream with id
+export const fetchStream = (id) => async (dispatch) => {
+	const response = await streams.get(`/streams/${id}`);
+
+	dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+// Important action creator (PUT request)
+export const editStream = (id, formValues) => async (dispatch) => {
+	const response = await streams.put(`/streams/${id}`, formValues);
+
+	dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+// To delete stream
+export const deleteStream = (id) => async (dispatch) => {
+	await streams.delete(`/streams/${id}`);
+
+	dispatch({ type: DELETE_STREAM, payload: id });
 };
