e80f4e90850ff974d5dfa6cbf0b5aed7a8f9db3f
rohan-04 <mathurrohan04@gmail.com>
2021-03-11 01:10:16 +0530

    created history

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {
 	SIGN_IN,
 	SIGN_OUT,
@@ -29,6 +30,9 @@ export const createStream = (formValues) => async (dispatch, getState) => {
 	const response = await streams.post('/streams', { ...formValues, userId });
 
 	dispatch({ type: CREATE_STREAM, payload: response.data });
+
+	// Programmatic navigation to get the user back to the root route
+	history.push('/');
 };
 
 // To fetch all streams
---
+++ src/history.js
@@ -0,0 +1,4 @@
+//It comes with react-router-dom
+import { createBrowserHistory } from 'history';
+
+export default createBrowserHistory();
